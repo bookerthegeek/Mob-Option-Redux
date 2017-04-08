4b6ac8f583d2765652a28aeb89fd82b642047f3b
Matryoshika <jonathancarlsson159@gmail.com>
2017-04-08 03:06:24 +0200

    Updating in-mod version number

--- src/main/java/com/bookerthegeek/moboptions/MobOptions.java
+++ src/main/java/com/bookerthegeek/moboptions/MobOptions.java
@@ -20,7 +20,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 )
 public class MobOptions{
     public static final String MODID = "moboptions";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
     
     @Instance("moboptions")
     public static MobOptions INSTANCE;
